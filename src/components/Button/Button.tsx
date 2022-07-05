c6301a7eae3a8d6343180453834d676f6c6075de
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-07-05 21:49:58 +0300

    Chores: add button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,24 @@
 import { Color } from "@/types/color";
 import classNames from "classnames";
 import React, { CSSProperties, FC } from "react";
+import buttonStyle from "./Button.module.scss";
 
 export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
     variant?: Color
 }
 
-const DEFAULT_CLASS_LIST: Array<string> = ["button"];
+const DEFAULT_CLASS_LIST: Array<string> = [buttonStyle.button];
 
 export const Button: FC<ButtonProps> = (props) => {
     const {children, variant, style, className, ...restProps} = props as ButtonProps;
 
-    const _className = classNames({
-        [DEFAULT_CLASS_LIST.join(" ")]: true,
-        [`button__${variant}`]: variant,
+    const _className = classNames(
+        DEFAULT_CLASS_LIST.join(" "),
+        `${buttonStyle.button}-${variant}` && variant,
         className
-    });
+    );
+
+    console.log(DEFAULT_CLASS_LIST);
 
     const _style: CSSProperties = style || {};    
 
